2bf151646da3457aff7f919e829b491ff74b4ff3
arnorbert <adriennorbert32@gmail.com>
2022-07-20 13:39:42 +0400

    ajout des modes de difficulte

--- script/plus_moins.c
+++ script/plus_moins.c
@@ -25,10 +25,40 @@ int main()
 	int valeur_a_trouver = rand() % 100 + 1;
 	int choix_joueur;
 	int resultat;
-	do
+	int mode;
+	int i = 0;
+	printf("Choisissez votre mode de difficulte:\n");
+	printf("1 pour facile\n");
+	printf("2 pour moyen\n");
+	printf("3 pour difficile\n");
+	scanf("%d",&mode);
+	if(mode == 1)
 	{
-		printf("Inserez un nombre entier pour essayer de trouver le nombre cacher: ");
-		scanf("%d",&choix_joueur);
-		resultat = plus_moins(choix_joueur,valeur_a_trouver);
-	}while(resultat<=1);
+		do
+		{
+			printf("Inserez un nombre entier pour essayer de trouver le nombre cacher: ");
+			scanf("%d",&choix_joueur);
+			resultat = plus_moins(choix_joueur,valeur_a_trouver);
+		}while(resultat<=1);
+	}
+	if(mode == 2)
+	{
+		do
+                {
+                        printf("Inserez un nombre entier pour essayer de trouver le nombre cacher: ");
+                        scanf("%d",&choix_joueur);
+                        resultat = plus_moins(choix_joueur,valeur_a_trouver);
+			i = i + 1;
+                }while(i<25);
+	}
+	if(mode == 3)
+	{
+		do
+                {
+                        printf("Inserez un nombre entier pour essayer de trouver le nombre cacher: ");
+                        scanf("%d",&choix_joueur);
+                        resultat = plus_moins(choix_joueur,valeur_a_trouver);
+			i = i + 1;
+                }while(i<10);
+	}
 }
